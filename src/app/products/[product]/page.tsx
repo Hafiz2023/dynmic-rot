0b3f0d8bfe3600d021ffcddf08655a63547a2f7a
Hafiz2023 <adilamin374@gmail.com>
2023-05-30 08:54:10 +0500

    update

---
+++ src/app/products/[product]/page.tsx
@@ -0,0 +1,10 @@
+export default function Product(){
+    return(
+        <>
+        <div>Product details</div>
+        <div> List 1</div>
+        <div> List 2</div>
+        <div> List 3</div>
+        </>
+    )
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,3 @@
+export default function Products() {
+  return <h3> Products list</h3>;
+}
